45025ff669584a614783363c5ac8ac04ccf93189
Parker Amundsen <parkera13@gmail.com>
2020-09-08 18:56:42 -0400

    Queue added tests to come

---
+++ PBAUtil/Queue.swift
@@ -0,0 +1,37 @@
+//
+//  Queue.swift
+//  PBAUtil
+//
+//  Created by parker amundsen on 9/8/20.
+//  Copyright © 2020 Parker Amundsen. All rights reserved.
+//
+
+import Foundation
+
+struct Queue<Element>: ExpressibleByArrayLiteral {
+    
+    typealias ArrayLiteralElement = Element
+    
+    private var elements: Array<Element>
+    
+    init(arrayLiteral elements: Element...) {
+        self.elements = elements
+    }
+    
+    init(_ array: Array<Element>) {
+        self.elements = array
+    }
+    
+    mutating func enqueue(_ element: Element) {
+        elements.append(element)
+    }
+    
+    mutating func dequeue() -> Element? {
+        return elements.isEmpty ? nil : elements.removeFirst()
+    }
+    
+    func getUnderlyingArray() -> Array<Element> {
+        return elements
+    }
+    
+}
